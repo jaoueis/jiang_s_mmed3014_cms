5eb6d4e332b3957dec30f0051deb0a8bc84e064b
Shan Jiang <shanjiang@Shan-Jiang.local>
2018-02-15 12:02:42 -0500

    set up connect php

---
+++ phpscripts/connection.php
@@ -0,0 +1,12 @@
+<?php
+$host     = "localhost";
+$user     = "root";
+$password = "root";
+$database = "db_users";
+$port     = 3306;
+
+$connect = mysqli_connect($host, $user, $password, $database, $port);
+
+if ($mysqli->connect_error) {
+    die('Error : (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
