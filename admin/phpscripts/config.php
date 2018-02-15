d6d4a944b5a238eb9b57f0b64ac3b982fefdd9a0
Shan Jiang <shanjiang@Shan-Jiang.local>
2018-02-15 11:51:41 -0500

    cleaned up all files and added gitignore

--- admin/phpscripts/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once('functions.php');
-require_once('read.php');
-require_once('login.php');
-require_once('sessions.php');
--- admin/phpscripts/sessions.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-function confirmLogin()
-{
-    if (!isset($_SESSION['user_id'])) {
-        redirect_to("admin_login.php");
-    }
-}
