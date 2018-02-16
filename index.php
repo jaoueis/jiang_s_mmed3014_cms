89da3037bed8b7aefbfe7eefb9bcf0b2f4d0a090
Shan Jiang <shanjiang@Shan-Jiang.local>
2018-02-16 01:22:18 -0500

    set up style and time compare

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if (isset($_POST['submit'])) {
         $result  = logIn($username, $password, $ipAddress, $currentDate);
         $message = $result;
     } else {
-        $message = "Please fill out the required field";
+        $message = "<span>Please fill out the required field.</span>";
     }
 }
 ?>
@@ -24,22 +24,34 @@ if (isset($_POST['submit'])) {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Document</title>
+<title>Log In</title>
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
+<div class="main-wrap">
+    <div class="msg-wrap">
+        <?php if (!empty($message)) {
+            echo $message;
+        } ?>
+    </div>
 
-<?php if (!empty($message)) {
-    echo $message;
-} ?>
-<form action="index.php" method="post">
-    <label for="username">Username:</label>
-    <input type="text" name="username">
+    <div class="form-wrap">
+        <img src="images/shan-logo-3.0-name.svg" alt="Logo">
+        <form action="index.php" method="post">
+            <div class="inputBox-wrap">
+                <label for="username">Username*</label>
+                <br>
+                <input type="text" name="username">
 
-    <label for="password">Password:</label>
-    <input type="password" name="password">
-
-    <input type="submit" name="submit" value="show me the money">
-</form>
-
+                <label for="password">Password*</label>
+                <br>
+                <input type="password" name="password">
+                <span>* is required</span>
+            </div>
+            <input type="submit" name="submit" value="Log In">
+        </form>
+    </div>
+</div>
 </body>
 </html>
--- welcome.php
+++ welcome.php
@@ -1,6 +1,14 @@
 <?php
 require_once('phpscripts/config.php');
 confirmLogin();
+
+date_default_timezone_set('America/Toronto');
+$currentTime = date("H:i:s");
+
+$midnight = "00:00:00";
+$dawn     = "06:00:00";
+$noon     = "12:00:00";
+$sunset   = "18:00:00";
 ?>
 
 <!doctype html>
@@ -9,11 +17,29 @@ confirmLogin();
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Document</title>
+<title>Welcome</title>
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-<?php echo "Welcome" . " " . $_SESSION['user_name']; ?>
-<br>
-<?php echo "Your last login was at " . $_SESSION['user_date']; ?>
+<div class="main-wrap">
+    <div class="info-wrap">
+        <span class="welcome">
+            <?php
+            if (time() >= strtotime($midnight) && time() <= strtotime($dawn)) {
+                echo "Good morning" . " " . $_SESSION['user_name'] . "! The early bird catches the worm!";
+            } else if (time() >= strtotime($dawn) && time() <= strtotime($noon)) {
+                echo "Hello" . " " . $_SESSION['user_name'] . "! Hope you have a great morning!";
+            } else if (time() >= strtotime($noon) && time() <= strtotime($sunset)) {
+                echo "Good afternoon" . " " . $_SESSION['user_name'] . "! What you up to?";
+            } else if (time() >= strtotime($sunset) && time() <= strtotime($midnight)) {
+                echo "Hi" . " " . $_SESSION['user_name'] . "! Hope you have a great evening!";
+            }
+            ?>
+        </span>
+        <br>
+        <span class="lastLogin"><?php echo "Your last login was at " . $_SESSION['user_date']; ?></span>
+    </div>
+</div>
 </body>
 </html>
