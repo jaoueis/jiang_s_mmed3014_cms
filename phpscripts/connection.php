948d1f0a4e8703c21cba94eaeed7f528a198ca69
sjiang@northern.co <sjiang@northern.co>
2018-02-21 10:09:53 -0500

    changed connect settings for Windows

--- phpscripts/connection.php
+++ phpscripts/connection.php
@@ -1,11 +1,11 @@
 <?php
 $host     = "localhost";
 $user     = "root";
-$pwd      = "root";
+$pwd      = ""; //for Mac the password is root
 $database = "db_users";
-$port     = 8889;
+// $port     = 8889; //comment out for Windows
 
-$connect = mysqli_connect($host, $user, $pwd, $database, $port);
+$connect = mysqli_connect($host, $user, $pwd, $database); //delete $port for Windows
 
 if ($connect->connect_error) {
     die('Error : (' . $connect->connect_errno . ') ' . $connect->connect_error);
