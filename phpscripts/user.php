908a94eda6a7a0500598b77ebcf30f9f74941bf7
sjiang@northern.co <sjiang@northern.co>
2018-02-21 13:43:03 -0500

    added mail function

--- phpscripts/user.php
+++ phpscripts/user.php
@@ -7,8 +7,14 @@ function createUser($fname, $username, $password, $email, $lvllist) {
 
     if ($userquery) {
         redirect_to('index.php');
+
+        $to      = $email;
+        $subject = "Welcome to Shan CMS!";
+        $content = "Hello " . $fname . "! You have successfully signed up for Shan CMS. Please log in on http://localhost. Your username is " . $username . " and your default password is " . $password . ". Please enjoy :)";
+
+        mail($to, $subject, $content);
     } else {
-        $message = "Failed";
+        $message = "Oops! Something went wrong.";
 
         return $message;
     }
