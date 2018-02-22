5193abe30a64b46321c5197f835744d40f9636f9
Shan Jiang <shanjiang@Shan-Jiang.local>
2018-02-21 22:38:35 -0500

    fixed email encrypt password bug

--- phpscripts/user.php
+++ phpscripts/user.php
@@ -2,14 +2,15 @@
 function createUser($fname, $username, $password, $email, $lvllist) {
     include('connection.php');
 
-    $password   = md5($password);
-    $userstring = "INSERT INTO tbl_user VALUES(NULL, '{$fname}', '{$username}', '{$password}', '{$email}', NULL, '{$lvllist}', 'no')";
-    $userquery  = mysqli_query($connect, $userstring);
+    $unencryptedPas = $password;
+    $password       = md5($password);
+    $userstring     = "INSERT INTO tbl_user VALUES(NULL, '{$fname}', '{$username}', '{$password}', '{$email}', NULL, '{$lvllist}', 'no')";
+    $userquery      = mysqli_query($connect, $userstring);
 
     if ($userquery) {
         $to      = $email;
         $subject = "Welcome to Shan CMS!";
-        $content = "Hello " . $fname . "! You have successfully signed up for Shan CMS. Please log in on http://localhost. Your username is " . $username . " and your default password is " . $password . ". Please enjoy :)";
+        $content = "Hello " . $fname . "! You have successfully signed up for Shan CMS. Please log in on http://localhost. Your username is " . $username . " and your default password is " . $unencryptedPas . ". Please enjoy :)";
 
         mail($to, $subject, $content);
 
