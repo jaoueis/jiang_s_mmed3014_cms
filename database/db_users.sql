06a6e777b0138fc1ca9e97f06f86c3906b9c30e8
Shan Jiang <shanjiang@Shan-Jiang.local>
2018-02-21 23:01:58 -0500

    updated database

--- database/db_users.sql
+++ database/db_users.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
 -- Host: localhost    Database: db_users
 -- ------------------------------------------------------
--- Server version	5.5.5-10.1.26-MariaDB
+-- Server version	5.6.35
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,10 +29,10 @@ CREATE TABLE `tbl_user` (
   `user_pass` varchar(250) NOT NULL,
   `user_email` varchar(250) NOT NULL,
   `user_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `user_level` int(11) NOT NULL,
+  `user_level` int(10) unsigned DEFAULT NULL,
   `user_ip` varchar(50) NOT NULL DEFAULT 'no',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +41,7 @@ CREATE TABLE `tbl_user` (
 
 LOCK TABLES `tbl_user` WRITE;
 /*!40000 ALTER TABLE `tbl_user` DISABLE KEYS */;
-INSERT INTO `tbl_user` VALUES (1,'Shan','sjiang','123456','jiang_shan@live.com','2018-02-21 18:21:02',0,'::1');
+INSERT INTO `tbl_user` VALUES (1,'admin','admin','admin','jiang_shan@live.com','2018-02-22 03:51:27',0,'::1'),(3,'Shan','sjiang','d88c28f713a1b0d28f5d038b586595a2','jiangshan1031@gmail.com','2018-02-22 02:57:04',2,'::1'),(4,'Jason','jason','48c1cecfee21ba8dca4f154d6174276f','123@test.com','2018-02-22 02:57:50',2,'::1'),(5,'Dolores','dol','4e9d856d23b3ddc84cf4fbcb4b494210','qwer@test.com','2018-02-22 03:52:29',2,'::1');
 /*!40000 ALTER TABLE `tbl_user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-02-21 13:24:51
+-- Dump completed on 2018-02-21 23:00:49
